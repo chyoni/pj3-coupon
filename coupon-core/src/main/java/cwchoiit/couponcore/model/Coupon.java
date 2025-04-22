e71a46337e46fb87ca4fb503ce307d332899f8ff
cwchoi <cwchoi@osci.kr>
2025-04-22 18:06:03 +0900

    Coupon Core - Entity 설계

---
+++ coupon-core/src/main/java/cwchoiit/couponcore/model/Coupon.java
@@ -0,0 +1,44 @@
+package cwchoiit.couponcore.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "coupon")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coupon_id")
+    private Long couponId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private CouponType couponType;
+
+    private Integer totalQuantity;
+
+    @Column(nullable = false)
+    private int issuedQuantity;
+
+    @Column(nullable = false)
+    private int discountAmount;
+
+    @Column(nullable = false)
+    private int minAvailableAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueStart;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueEnd;
+}
