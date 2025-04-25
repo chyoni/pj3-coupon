cfe1b7474a8d8925b3ae09a177e9dbd082406e9d
cwchoi <cwchoi@osci.kr>
2025-04-25 15:13:41 +0900

    분산 락이 아닌 비관적 락을 사용한 락
    - SELECT ... FOR UPDATE

--- coupon-core/src/main/java/cwchoiit/couponcore/repository/mysql/CouponRepository.java
+++ coupon-core/src/main/java/cwchoiit/couponcore/repository/mysql/CouponRepository.java
@@ -2,6 +2,16 @@ package cwchoiit.couponcore.repository.mysql;
 
 import cwchoiit.couponcore.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    @Query(
+            nativeQuery = true,
+            value = "select * from coupon where coupon_id = :couponId for update"
+    )
+    Optional<Coupon> findLockedByCouponId(@Param("couponId") Long couponId);
 }
